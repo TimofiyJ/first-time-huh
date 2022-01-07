ab6cbadf023ee092e4912e4c7777629d5391cded
gtfotimmi <44300490+TimofiyJ@users.noreply.github.com>
2022-01-07 20:30:14 +0200

    Update func.c

--- path/func.c
+++ path/func.c
@@ -2,37 +2,33 @@
 #include <stdlib.h>
 
 
-void fillMatrix(int **matrix, int n){
-    int k=0;
-    int size=n;
-    while(n!=0){
-        int* M = (int*)malloc(n*sizeof(int));
+void fillMatrix(int **matrix, int size){
+    int position=0;
+    int sizecurr=size;
+    while(size!=0){
+        int* M = (int*)malloc(sizecurr*sizeof(int));
         for(int i=0; i<size;i++){
             scanf("%d",&M[i]);
         }
-        matrix[k]=M;
-        k=k+1;
-        n=n-1;
+        matrix[position]=M;
+        position=position+1;
+        size=size-1;
     }
 }
 
-void printMatrix(int **matrix, int n){
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+void printMatrix(int **matrix, int size){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
             printf("%d ",matrix[i][j]);
         }
         printf("\n");
     }
 }
 
-void freeMatrix(int** matrix, int n){
-    for(int i=0;i<n;i++){
+void freeMatrix(int** matrix, int size){
+    for(int i=0;i<size;i++){
         free(matrix[i]);
     }
     free(matrix);
 }
-
-//
-// Created by 1 on 05.01.2022.
-//
 
