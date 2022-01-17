908e1c0f9bddce178f8b8b13b930b9d0d08eb9f7
gtfotimmi <44300490+TimofiyJ@users.noreply.github.com>
2022-01-17 22:09:52 +0200

    Update func.c

--- path/func.c
+++ path/func.c
@@ -5,12 +5,14 @@
 void fillMatrix(int **matrix, int size){
     int position=0;
     int sizecurr=size;
+    int row=1;
     while(size!=0){
         int* M = (int*)malloc(sizecurr*sizeof(int));
         for(int i=0; i<sizecurr;i++){
-            printf("Enter %d element \n",i+1);
+            printf("Enter %d element %d row \n",i+1, row);
             scanf("%d",&M[i]);
         }
+        row=row+1;
         matrix[position]=M;
         position=position+1;
         size=size-1;
@@ -31,4 +33,26 @@ void freeMatrix(int** matrix, int size){
         free(matrix[i]);
     }
     free(matrix);
+}
+
+void searchMatrix(int** matrix, int size){
+    int startrow,startcolumn,endrow,endcolumn;
+    for(int i=0;i<size;i++){
+        for(int j=0; j<size;j++){
+            if (matrix[i][j]==0){
+                startcolumn=j;
+                startrow=i;
+            }
+            if (matrix[i][j]==-2){
+                endcolumn=j;
+                endrow=i;
+            }
+        }
+    }
+    while(1){
+        if (startcolumn+1<size){
+            matrix[startrow][startcolumn+1]=matrix[startrow][startcolumn]+matrix[startrow][startcolumn+1];
+        }
+    }
+
 }
