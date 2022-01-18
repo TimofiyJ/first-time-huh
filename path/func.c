bcee6ed0b66241ccebc39a05bc35f154b05edfef
gtfotimmi <44300490+TimofiyJ@users.noreply.github.com>
2022-01-18 14:51:37 +0200

    Update func.c

--- path/func.c
+++ path/func.c
@@ -43,69 +43,6 @@ void searchMatrix(int** matrix, int size){
                 startcolumn=j;
                 startrow=i;
             }
-            if (matrix[i][j]==-2){
-                endcolumn=j;
-                endrow=i;
-            }
-        }
-    }
-    while(1){
-        if (startcolumn+1<size){
-            matrix[startrow][startcolumn+1]=matrix[startrow][startcolumn]+matrix[startrow][startcolumn+1];
-        }
-    }
-
-}
-
-
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-void fillMatrix(int **matrix, int size){
-    int position=0;
-    int sizecurr=size;
-    int row=1;
-    while(size!=0){
-        int* M = (int*)malloc(sizecurr*sizeof(int));
-        for(int i=0; i<sizecurr;i++){
-            printf("Enter %d element %d row \n",i+1, row);
-            scanf("%d",&M[i]);
-        }
-        row=row+1;
-        matrix[position]=M;
-        position=position+1;
-        size=size-1;
-    }
-}
-
-void printMatrix(int **matrix, int size){
-    for(int i=0;i<size;i++){
-        for(int j=0;j<size;j++){
-            printf("%d ",matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void freeMatrix(int** matrix, int size){
-    for(int i=0;i<size;i++){
-        free(matrix[i]);
-    }
-    free(matrix);
-}
-
-void searchMatrix(int** matrix, int size){
-    int startrow,startcolumn,endrow,endcolumn;
-    for(int i=0;i<size;i++){
-        for(int j=0; j<size;j++){
-            if (matrix[i][j]==0){
-                startcolumn=j;
-                startrow=i;
-            }
             if (matrix[i][j]==-1){
                 endcolumn=j;
                 endrow=i;
@@ -177,10 +114,16 @@ void searchMatrix(int** matrix, int size){
         printf("\nposmin: %d\n",posmin);
         startrow=posmin/size;
         startcolumn=posmin%size;
-        if (startrow==endrow && startcolumn==endcolumn){
-            printf("%d", distance[posmin]);
+        int sum=size*size;
+        int sum2=0;
+        for(int i=0;i<size*size;i++){
+            sum2=sum2+marked[i];
+        }
+        if(sum2==sum){
             break;
         }
+
     }
+    printf("%d",distance[endrow*size+endcolumn]);
 
 }
