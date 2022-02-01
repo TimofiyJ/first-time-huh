e1cadf964d0de95f1f94aa023f7a9fce55b410c3
gtfotimmi <44300490+TimofiyJ@users.noreply.github.com>
2022-02-01 12:20:25 +0200

    Update main.c

--- path/main.c
+++ path/main.c
@@ -1,25 +1,70 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <stdlib.h>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
 void fillMatrix(int**, int);
 void printMatrix(int**, int);
 void freeMatrix(int**, int);
 void searchMatrix(int**, int);
-void fillArrays(int** marked, int** distance ,int size,int startrow, int startcolumn);
+
+class point{
+public:
+    point(int mark) {
+        number = mark;
+    }
+    void scanType(int mark) {
+        if (mark == -1) {
+            type = "end";
+            value = 1;
+        }
+        if (mark > 0) {
+            type = "point";
+            value = mark;
+        }
+        if (mark == -2) {
+            type = "wall";
+            value = -1;
+        }
+        if (mark == 0) {
+            type = "beginning";
+            value = 0;
+        }
+
+    }
+    int getValue(int mark) {
+        return value;
+    }
+    string getType(int mark) {
+        return type;
+    }
+private:
+    int value;
+    int number;
+    string type;
+};
+
 int main() {
     int size;
 
-    //scanning the size
-    printf("Please, enter size of the matrix\n");
-    scanf("%d",&size);
-    //
 
-    //filling matrix
-    int** Matrix = (int**)malloc(size*sizeof(int));
+    cout << "Please, enter size of the matrix \n" << endl;
+    cin >> size;
+ 
+
+ 
+    int** Matrix = new int*[size];
     fillMatrix(Matrix, size);
 
-    printMatrix(Matrix,size);
-    searchMatrix(Matrix,size);
+    printMatrix(Matrix, size);
 
-    freeMatrix(Matrix,size);
+
+    searchMatrix(Matrix, size);
+
+    freeMatrix(Matrix, size);
     return 0;
 }
